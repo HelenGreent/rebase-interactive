956bb02f7ccf3b0c1ebbd524645cee7b19ce47ab
Helen <helengreent23@gmail.com>
2022-04-07 22:13:16 +0300

    task1

---
+++ task1.js
@@ -0,0 +1,23 @@
+function calculate(equation) {
+    equation = equation.split(" ");
+    let [a, operator, b] = equation;
+    switch (operator) {
+      case "+":
+        equation = a.length + b.length;
+        break;
+      case "-":
+        equation = a.length - b.length;
+        break;
+      case "*":
+        equation = a.length * b.length;
+        break;
+      case "//":
+        equation = Math.trunc(a.length / b.length);
+        break;
+    }
+    a = [];
+    for (let i = equation; i > 0; i--) {
+      a.push(".");
+    }
+    return a.join("");
+  }
